d3cf1a9db2fc2f6492f5ddbab8c5fa3bc1f22338
Monk <tbsanders5@gmail.com>
2020-09-26 17:45:56 -0700

    finished working update role function

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ var connection = mysql.createConnection({
                         break;
                     case "Add Department":
                         addDepartment();
+                        break;
+                    case "Add Role":
+                        addNewRole();
+                        break;
+                    case "Update Employee Role":
+                        updateEmployeesRole();
+                        break;    
+                    case "exit":
+                        connection.end();
                         break;    
                 }
             });
@@ -60,9 +69,12 @@ var connection = mysql.createConnection({
                 if (err) throw err;
                     console.table(res);
                     runProgram();
+
+                    return res;
                 
             });
   };
+
 
   function deptList() {
       connection.query("SELECT * from department", function(err, res) {
@@ -144,4 +156,92 @@ var connection = mysql.createConnection({
           });
       });
   };
+
+  function addNewRole() {
+    const questions = [
+        {
+            type: "input",
+            message: "Enter the role you would like to add",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter the department this role will be in by ID number",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter the employees salary",
+            name: "salary"
+        }
+    ];
+    inquirer.prompt(questions).then(function(answer) {
+        connection.query(
+            "INSERT INTO roles SET ?",
+            {
+                title: answer.title,
+                department_id: answer.id,
+                salary: answer.salary
+            },
+            function(err, res) {
+                if(err) throw err;
+                runProgram();
+            }
+        );
+    });
+};
+
+    function updateEmployeesRole() {
+        let importEmployeeArray = [];
+        connection.query("SELECT employees_id, concat(first_name, ' ', last_name) AS NAME FROM employee_db.employees", (err, rows) => {
+            if(err) throw err;
+            rows.forEach((row) => {
+                let employeeObject = {
+                    name: row.NAME,
+                    value: row.id
+                }
+                importEmployeeArray.push(employeeObject);
+            });
+
+            let importRoleArray = [];
+            connection.query("SELECT roles.title AS ROLE, roles_id as roles_id FROM employee_db.roles AS roles", (err, rows) => {
+                if(err) throw err;
+                rows.forEach((row) => {
+                    let roleObject = {
+                        name: row.ROLE,
+                        value: row.roles_id
+                    }
+                    importRoleArray.push(roleObject)
+                });
+                inquirer
+                .prompt([
+                    {
+                        type: "list",
+                        name: "empList",
+                        message: "Choose employee to change role",
+                        choices: importEmployeeArray
+                    },
+                    {
+                        type: "list",
+                        name: "roles_title",
+                        message: "Choose role to assign to employee",
+                        choices: importRoleArray
+                    }
+                ])
+                .then(answers => {
+                    const empList = answers.empList;
+                    const roles_title = answers.roles_title;
+                    connection.query(
+                        `UPDATE employee_db.employees SET roles_id = "${roles_title}" WHERE employees_id = "${empList}"`,
+                        function(err, res) {
+                            if (err) throw err;
+                            console.log("\n");
+                            console.log("Updated Employee")
+                            runProgram();
+
+                        });
+                })
+            });
+        });
+    }
 
